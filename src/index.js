b130675fc954281f8c7c20ad7d7f363ac83ad33c
Maxim Demenkow <demenkow.maxim@gmail.com>
2019-10-05 18:31:59 +0300

    feat: solve how many weeks need studying

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = {
+    family: 4,
+    friends: 10,
+    normal_life: 20,
+    profession: 30,
+    carrier: 40,
+    top_peformance: 60
+  }
+) {
+  //get how many hours need to learn
+  var hours = 0;
+  if (knowsProgramming === true) {
+    hours = 800;
+  }
+  else {
+    hours = 1300;
+  }
+  //get how many weeks need to learn (Priorities affect to learning time)
+  return Math.ceil(hours / config[focus]);
+};
